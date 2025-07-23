f6cf8129cda6e41a04c1a2426510ba655867b792
aaronkho <aaronkho@mit.edu>
2025-07-23 19:15:20 -0400

    Added functions to estimate COCOS number

--- src/fbe/eqdsk.py
+++ src/fbe/eqdsk.py
@@ -1,4 +1,7 @@
+import copy
 from pathlib import Path
+from typing import Any, Final, Self
+from collections.abc import MutableMapping, Mapping, MutableSequence, Sequence, Iterable
 import numpy as np
 
 array_types = (list, tuple, np.ndarray)
@@ -85,8 +88,8 @@ def read_eqdsk_file(fname):
         eq['rbdry'] = bbbs[:, 0]
         eq['zbdry'] = bbbs[:, 1]
     else:
-        eq['rbdry'] = None
-        eq['zbdry'] = None
+        eq['rbdry'] = np.array([])
+        eq['zbdry'] = np.array([])
 
     # Read limiter vector
     if eq['nlim'] > 0:
@@ -95,8 +98,8 @@ def read_eqdsk_file(fname):
         eq['rlim'] = lim[:, 0]
         eq['zlim'] = lim[:, 1]
     else:
-        eq['rlim'] = None
-        eq['zlim'] = None
+        eq['rlim'] = np.array([])
+        eq['zlim'] = np.array([])
 
     return eq
 
@@ -245,8 +248,8 @@ def write_eqdsk_file(
     dummy = 0.0
 
     with open(fpath, 'w') as ff:
-        gcase = case[:48] if len(case) > 48 else case
-        ff.write(f'{gcase:-48s}{idum:4d}{nr:4d}{nz:4d}\n')
+        gcase = gcase[:48] if len(gcase) > 48 else gcase
+        ff.write(f'{gcase:<48}{gid:4d}{nr:4d}{nz:4d}\n')
         ff.write(f'{rdim:16.9E}{zdim:16.9E}{rcentr:16.9E}{rleft:16.9E}{zmid:16.9E}\n')
         ff.write(f'{rmagx:16.9E}{zmagx:16.9E}{simagx:16.9E}{sibdry:16.9E}{bcentr:16.9E}\n')
         ff.write(f'{cpasma:16.9E}{simagx:16.9E}{dummy:16.9E}{rmagx:16.9E}{dummy:16.9E}\n')
@@ -281,18 +284,18 @@ def write_eqdsk_file(
         ff.write('\n')
         for ii in range(len(qpsi)):
             ff.write(f'{qpsi[ii]:16.9E}')
-            if (ii + 1) % 5 == 0 and (ii + 1) != len(q):
+            if (ii + 1) % 5 == 0 and (ii + 1) != len(qpsi):
                 ff.write('\n')
         ff.write('\n')
         ff.write(f'{nbdry:5d}{nlim:5d}\n')
         kk = 0
         for ii in range(nbdry):
             ff.write(f'{rbdry[ii]:16.9E}')
-            if (kk + 1) % 5 == 0 and (ii + 1) != nbry:
+            if (kk + 1) % 5 == 0 and (ii + 1) != nbdry:
                 ff.write('\n')
             kk += 1
             ff.write(f'{zbdry[ii]:16.9E}')
-            if (kk + 1) % 5 == 0 and (ii + 1) != nbry:
+            if (kk + 1) % 5 == 0 and (ii + 1) != nbdry:
                 ff.write('\n')
             kk += 1
         if kk > 0:
@@ -304,8 +307,136 @@ def write_eqdsk_file(
                 ff.write('\n')
             kk += 1
             ff.write(f'{zlim[ii]:16.9E}')
-            if (kk + 1) % 5 == 0 and (kk + 1) != limitr:
+            if (kk + 1) % 5 == 0 and (kk + 1) != nlim:
                 ff.write('\n')
             kk += 1
     print(f'Output EQDSK file saved as {fpath}.')
 
+
+def define_cocos(cocos_number: int) -> MutableMapping[str, int]:
+    # Default dictionary returns COCOS=1
+    sign_dict = {
+        'eBp': 0,   # Normalization of flux by 2 pi
+        'sBp': 1,   # Increasing or decreasing flux from axis
+        'scyl': 1,  # Handedness of cylindrical coordinates
+        'spol': 1,  # Handedness of poloidal coordinates
+        'srel': 1,  # Swapping of handedness between cylindrical and poloidal
+    }
+    if cocos_number < 0:
+        cocos_number = -cocos_number
+        sign_dict['srel'] = -1
+    if cocos_number > 10:
+        cocos_number -= 10
+        sign_dict['eBp'] = 1
+    if cocos_number in [3, 4, 7, 8]:
+        sign_dict['sBp'] = -1
+    if cocos_number in [2, 4, 6, 8]:
+        sign_dict['scyl'] = -1
+    if cocos_number in [3, 4, 5, 6]:
+        sign_dict['spol'] = -1
+    return sign_dict
+
+
+def define_cocos_converter(cocos_in: int, cocos_out: int) -> MutableMapping[str, int]:
+    in_dict = define_cocos(cocos_in)
+    out_dict = define_cocos(cocos_out)
+    for key in out_dict:
+        if key == 'eBp':
+            out_dict[key] -= in_dict[key]
+        else:
+            out_dict[key] *= in_dict[key]
+    return out_dict
+
+
+def determine_cocos(sign_dict: MutableMapping[str, int]) -> int:
+    cocos_number = 0  # Signifies unknown
+    fcomplete = True
+    for var in ['eBp', 'sBp', 'scyl', 'spol', 'srel']:
+        if var not in sign_dict:
+            fcomplete = False
+    if fcomplete:
+        cocos_number = 1
+        if sign_dict['sBp'] * sign_dict['spol'] < 0:
+            cocos_number += 4
+        if sign_dict['sBp'] < 0:
+            cocos_number += 2
+        if sign_dict['scyl'] == 0:
+            print('Ambiguous cylindrical direction, assuming ccw from top')
+        elif sign_dict['scyl'] < 0:
+            cocos_number += 1
+        if sign_dict['eBp'] < 0:
+            print('Ambiguous per radian specification, assuming not per radian')
+        elif sign_dict['eBp'] > 0:
+            cocos_number += 10
+        if sign_dict['srel'] == 0:
+            print('Ambiguous relative coordinate handedness, assuming all right-handed')
+        if sign_dict['srel'] < 0:
+            cocos_number = -cocos_number
+    return cocos_number
+
+
+def detect_cocos(eqdsk: MutableMapping[str, Any]) -> int:
+    sign_dict = {}
+    sIp = int(np.sign(eqdsk['cpasma'])) if 'cpasma' in eqdsk else 0
+    sBt = int(np.sign(eqdsk['bcentr'])) if 'bcentr' in eqdsk else 0
+    if sIp != 0 and sBt != 0:
+        sign_dict['scyl'] = 0
+        sign_dict['eBp'] = -1
+        sign_dict['srel'] = 0
+        if 'sibdry' in eqdsk and 'simagx' in eqdsk:
+            sign_dict['sBp'] = int(np.sign(eqdsk['sibdry'] - eqdsk['simagx'])) * sIp
+        if 'qpsi' in eqdsk:
+            sign_dict['spol'] = int(np.sign(eqdsk['qpsi'][-1])) * sIp * sBt
+    return determine_cocos(sign_dict)
+
+
+def convert_cocos(eqdsk: MutableMapping[str, Any], cocos_in: int, cocos_out: int, bt_sign_out: int | None = None, ip_sign_out: int | None = None) -> MutableMapping[str, Any]:
+    out = {
+        'nr': eqdsk.get('nr', None),
+        'nz': eqdsk.get('nz', None),
+        'rdim': eqdsk.get('rdim', None),
+        'zdim': eqdsk.get('zdim', None),
+        'rcentr': eqdsk.get('rcentr', None),
+        'bcentr': eqdsk.get('bcentr', None),
+        'rleft': eqdsk.get('rleft', None),
+        'zmid': eqdsk.get('zmid', None),
+        'rmagx': eqdsk.get('rmagx', None),
+        'zmagx': eqdsk.get('zmagx', None),
+        'cpasma': eqdsk.get('cpasma', None),
+    }
+    sign_dict = define_cocos_converter(cocos_in, cocos_out)
+    sIp = sign_dict['scyl']
+    sBt = sign_dict['scyl']
+    if 'bcentr' in eqdsk:
+        out['bcentr'] = copy.deepcopy(eqdsk['bcentr']) * sBt
+        if bt_sign_out is not None:
+            out['bcentr'] *= np.sign(out['bcentr']) * np.sign(bt_sign_out)
+            sBt *= int(np.sign(out['bcentr']) * np.sign(bt_sign_out))
+    if 'cpasma' in eqdsk:
+        out['cpasma'] = copy.deepcopy(eqdsk['cpasma']) * sIp
+        if ip_sign_out is not None:
+            out['cpasma'] *= np.sign(out['cpasma']) * np.sign(ip_sign_out)
+            sIp *= int(np.sign(out['cpasma']) * np.sign(ip_sign_out))
+    if 'simagx' in eqdsk:
+        out['simagx'] = copy.deepcopy(eqdsk['simagx']) * np.power(2.0 * np.pi, sign_dict['eBp']) * sign_dict['sBp'] * sIp
+    if 'sibdry' in eqdsk:
+        out['sibdry'] = copy.deepcopy(eqdsk['sibdry']) * np.power(2.0 * np.pi, sign_dict['eBp']) * sign_dict['sBp'] * sIp
+    if 'fpol' in eqdsk:
+        out['fpol'] = copy.deepcopy(eqdsk['fpol']) * sBt
+    if 'pres' in eqdsk:
+        out['pres'] = copy.deepcopy(eqdsk['pres'])
+    if 'ffprime' in eqdsk:
+        out['ffprime'] = copy.deepcopy(eqdsk['ffprime']) * np.power(2.0 * np.pi, -sign_dict['eBp']) * sign_dict['sBp'] * sIp
+    if 'pprime' in eqdsk:
+        out['pprime'] = copy.deepcopy(eqdsk['pprime']) * np.power(2.0 * np.pi, -sign_dict['eBp']) * sign_dict['sBp'] * sIp
+    if 'psi' in eqdsk:
+        out['psi'] = copy.deepcopy(eqdsk['psi']) * np.power(2.0 * np.pi, sign_dict['eBp']) * sign_dict['sBp'] * sIp
+    if 'qpsi' in eqdsk:
+        out['qpsi'] = copy.deepcopy(eqdsk['qpsi']) * sign_dict['spol'] * sIp * sBt
+    if 'rlim' in eqdsk and 'zlim' in eqdsk:
+        out['rlim'] = copy.deepcopy(eqdsk['rlim'])
+        out['zlim'] = copy.deepcopy(eqdsk['zlim'])
+    if 'rbdry' in eqdsk and 'zbdry' in eqdsk:
+        out['rbdry'] = copy.deepcopy(eqdsk['rbdry'])
+        out['zbdry'] = copy.deepcopy(eqdsk['zbdry'])
+    return out
