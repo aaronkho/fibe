594322417b2ff18b0e9511d108d02bc666631418
Aaron Ho <aaron.k.hohoho@gmail.com>
2025-08-24 22:11:21 -0400

    Updated scripts to improve user reusability

--- src/fibe/scripts/create_from_f_p_boundary.py
+++ src/fibe/scripts/create_from_f_p_boundary.py
@@ -2,13 +2,22 @@ import argparse
 from fibe import FixedBoundaryEquilibrium
 
 
-def main():
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
+def compute_from_scratch_using_f_p_boundary():
     eq = FixedBoundaryEquilibrium()
     eq.define_grid(args.nr, args.nz, args.rmin, args.rmax, args.zmin, args.zmax)
     eq.define_boundary(args.rb, args.zb)
-    eq.setup()
-    eq.init_psi()
-    eq.run(args.niter, args.err, args.relax, args.relaxj)
+    eq.initialize_psi()
+    eq.solve_psi(args.niter, args.err, args.relax, args.relaxj)
+    return eq
+
+
+def main():
+    args = parse_command_line_arguments()
 
 
 if __name__ == '__main__':
--- src/fibe/scripts/regrid_eqdsk.py
+++ src/fibe/scripts/regrid_eqdsk.py
@@ -2,7 +2,7 @@ import argparse
 from fibe import FixedBoundaryEquilibrium
 
 
-def main():
+def parse_command_line_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('ifile', type=str, required=True, help='Path to input g-eqdsk file')
     parser.add_argument('nr', type=int, required=True, help='Number of grid points in R')
@@ -13,18 +13,26 @@ def main():
     parser.add_argument('--relaxj', type=float, defualt=1.0, help='Relaxation constant to smoothen current stepping for stability')
     parser.add_argument('--ofile', type=str, default=None, help='Path for output g-eqdsk file')
     parser.add_arguemnt('--optimize', default=False, action='store_true', help='Toggle on optimal grid dimensions to fit boundary contour')
-    parser.add_argument('--keep_psi_scale', default=False, action='store_true', help='Toggle on renormalization of psi solution to original psi scale')
-    args = parser.parse_args()
+    #parser.add_argument('--keep_psi_scale', default=False, action='store_true', help='Toggle on renormalization of psi solution to original psi scale')
+    return parser.parse_args()
 
+
+def regrid_geqdsk(ipath, niter, tol, relax, relaxj):
+    eq = FixedBoundaryEquilibrium.from_geqdsk(ipath)
+    eq.regrid(args.nr, args.nz, optimal=args.optimize)
+    eq.solve_psi(niter, tol, relax, relaxj)
+    return eq
+
+
+def main():
+    args = parse_command_line_arguments()
     ipath = Path(args.ifile)
     if ipath.is_file():
-        eq = FixedBoundaryEquilibrium.from_eqdsk(ipath)
-        eq.regrid(args.nr, args.nz, optimal=args.optimize)
-        eq.solve_psi(args.niter, args.tol, args.relax, args.relaxj)
+        eq = regrid_geqdsk(ipath, args.niter, args.tol, args.relax, args.relaxj)
         opath = Path(args.ofile) if args.ofile is not None else ifile.resolve().parent / 'fibe_regridded_input.geqdsk'
         if not opath.exists():
             opath.parent.mkdir(parents=True, exist_ok=True)
-            eq.to_eqdsk(opath)
+            eq.to_geqdsk(opath)
 
 
 if __name__ == '__main__':
